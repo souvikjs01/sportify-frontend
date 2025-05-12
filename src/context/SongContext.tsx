a1b8932c83bdc58fe55762ad2a8ba83dfc46e63a
souvikjs01 <souvik741156@gmail.com>
2025-05-12 15:18:03 +0530

    fixed

--- src/context/SongContext.tsx
+++ src/context/SongContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useCallback, useContext, useEffect, useState } from "react";
 import type { Album, Song } from "../lib/types";
 import axios from "axios";
-const server = "http://13.232.120.206:8080";
+const server = "http://13.232.120.206";
 
 
 interface SongContextType {
@@ -45,7 +45,7 @@ export const SongProvider: React.FC<SongProviderProps> = ({children}) => {
 
     const fetchSongs = useCallback(async () => {
         try {
-            const { data } = await axios.get<Song[]>(`${server}/api/v1/song/all`);
+            const { data } = await axios.get<Song[]>(`${server}/api/v2/song/all`);
             setSongs(data);
             if(data.length > 0) {
                 setSelectedSong(data[0].id.toString())
@@ -60,7 +60,7 @@ export const SongProvider: React.FC<SongProviderProps> = ({children}) => {
 
     const fetchAlbums = useCallback(async () => {
         try {
-            const { data } = await axios.get<Album[]>(`${server}/api/v1/album/all`);
+            const { data } = await axios.get<Album[]>(`${server}/api/v2/album/all`);
             setAlbums(data)
         } catch (error) {
             console.log(error);
@@ -90,7 +90,7 @@ export const SongProvider: React.FC<SongProviderProps> = ({children}) => {
     const fetchSingleSong = useCallback(async () => {
         try {
             if(!selectedSong) return;
-            const { data } = await axios.get<Song>(`${server}/api/v1/song/${selectedSong}`);
+            const { data } = await axios.get<Song>(`${server}/api/v2/song/${selectedSong}`);
             setSong(data)
         } catch (error) {
             console.log(error);
@@ -101,7 +101,7 @@ export const SongProvider: React.FC<SongProviderProps> = ({children}) => {
 
     const fetchAlbumsongs = useCallback(async (id: string) => {
         try {
-            const { data } = await axios.get<{songs: Song[]; album: Album}>(`${server}/api/v1/album/${id}`);
+            const { data } = await axios.get<{songs: Song[]; album: Album}>(`${server}/api/v2/album/${id}`);
             console.log(data);
             setAlbumSongs(data.songs);
             setAlbumData(data.album);        
